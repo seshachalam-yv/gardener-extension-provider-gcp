704d162ecfe2c7f9f21a05fef5b165fc1392312b
Yerasala Venkata, Seshachalam <seshachalam.yerasala.venkata@sap.com>
2024-11-20 17:33:35 +0530

    Added a new `SeedValidator` to validate immutable settings in the Seed's backup configuration.

---
+++ pkg/admission/validator/seed.go
@@ -0,0 +1,117 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package validator
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/gardener/gardener/pkg/apis/core"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+
+	gcpclient "github.com/gardener/gardener-extension-provider-gcp/pkg/gcp/client"
+	"github.com/gardener/gardener/extensions/pkg/webhook"
+)
+
+// NewSeedValidator returns a new Validator for Seed resources,
+// ensuring backup configuration immutability according to policy.
+func NewSeedValidator(mgr manager.Manager) webhook.Validator {
+	return &seedValidator{
+		client:  mgr.GetClient(),
+		decoder: serializer.NewCodecFactory(mgr.GetScheme()).UniversalDecoder(),
+	}
+}
+
+// seedValidator validates create and update operations on Seed resources,
+// enforcing immutability of backup configurations.
+type seedValidator struct {
+	client  client.Client
+	decoder runtime.Decoder
+}
+
+// Validate validates the Seed resource during create or update operations.
+// It enforces immutability policies on backup configurations to prevent
+// disabling immutable settings, reducing retention periods, or changing retention types.
+func (s *seedValidator) Validate(ctx context.Context, newObj, oldObj client.Object) error {
+	newSeed, ok := newObj.(*core.Seed)
+	if !ok {
+		return fmt.Errorf("wrong object type %T for new object", newObj)
+	}
+
+	if oldObj != nil {
+		oldSeed, ok := oldObj.(*core.Seed)
+		if !ok {
+			return fmt.Errorf("wrong object type %T for old object", oldObj)
+		}
+		return s.validateUpdate(ctx, oldSeed, newSeed)
+	}
+
+	return s.validateCreate(ctx, newSeed)
+}
+
+// validateCreate validates the Seed object upon creation.
+// It checks if immutable settings are provided and validates them to ensure they meet the required criteria.
+func (s *seedValidator) validateCreate(_ context.Context, newSeed *core.Seed) error {
+	newImSettings, err := gcpclient.ParseImmutableSettings(newSeed.Spec.Backup.ProviderConfig)
+	if err != nil {
+		return fmt.Errorf("error parsing immutable settings: %w", err)
+	}
+
+	// If immutable settings are provided, validate them
+	if newImSettings != nil {
+		if err := gcpclient.ValidateImmutableSettings(newImSettings); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// validateUpdate validates the Seed object during an update operation.
+// It ensures that immutability policies are enforced by preventing:
+// - Disabling immutable settings once they are enabled.
+// - Reducing the retention period.
+// - Modifying the retention type.
+func (s *seedValidator) validateUpdate(_ context.Context, oldSeed, newSeed *core.Seed) error {
+	oldImSettings, err := gcpclient.ParseImmutableSettings(oldSeed.Spec.Backup.ProviderConfig)
+	if err != nil {
+		return fmt.Errorf("error parsing old immutable settings: %w", err)
+	}
+	newImSettings, err := gcpclient.ParseImmutableSettings(newSeed.Spec.Backup.ProviderConfig)
+	if err != nil {
+		return fmt.Errorf("error parsing new immutable settings: %w", err)
+	}
+
+	// Ensure that immutable settings are not disabled
+	if newImSettings == nil && oldImSettings != nil {
+		return errors.New("disabling immutable settings is not allowed")
+	}
+
+	// Ensure the retention period is not reduced
+	if newImSettings != nil && oldImSettings != nil {
+		if newImSettings.RetentionPeriod < oldImSettings.RetentionPeriod {
+			return fmt.Errorf("reducing the retention period from %s to %s is not allowed", oldImSettings.RetentionPeriod, newImSettings.RetentionPeriod)
+		}
+
+		// Ensure the retention type is not changed
+		if newImSettings.RetentionType != oldImSettings.RetentionType {
+			return fmt.Errorf("modifying the retention type from '%s' to '%s' is not allowed", oldImSettings.RetentionType, newImSettings.RetentionType)
+		}
+	}
+
+	// If immutable settings are newly added, validate them
+	if oldImSettings == nil && newImSettings != nil {
+		if err := gcpclient.ValidateImmutableSettings(newImSettings); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
---
+++ pkg/admission/validator/seed_test.go
@@ -0,0 +1,209 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package validator_test
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/gardener/gardener-extension-provider-gcp/pkg/admission/validator"
+	extensionswebhook "github.com/gardener/gardener/extensions/pkg/webhook"
+	mockclient "github.com/gardener/gardener/third_party/mock/controller-runtime/client"
+	mockmanager "github.com/gardener/gardener/third_party/mock/controller-runtime/manager"
+	"go.uber.org/mock/gomock"
+
+	"github.com/gardener/gardener/pkg/apis/core"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+var _ = Describe("Seed Validator", func() {
+	var (
+		mgr           *mockmanager.MockManager
+		seedValidator extensionswebhook.Validator
+		ctrl          *gomock.Controller
+		c             *mockclient.MockClient
+	)
+
+	BeforeEach(func() {
+		ctrl = gomock.NewController(GinkgoT())
+
+		scheme := runtime.NewScheme()
+		Expect(core.AddToScheme(scheme)).To(Succeed())
+		mgr = mockmanager.NewMockManager(ctrl)
+		mgr.EXPECT().GetScheme().Return(scheme).Times(1)
+		mgr.EXPECT().GetClient().Return(c)
+		seedValidator = validator.NewSeedValidator(mgr)
+	})
+
+	DescribeTable("ValidateUpdate",
+		func(oldSeed, newSeed *core.Seed, expectedError error) {
+			err := seedValidator.Validate(context.Background(), newSeed, oldSeed)
+			if expectedError != nil {
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(Equal(expectedError.Error()))
+			} else {
+				Expect(err).NotTo(HaveOccurred())
+			}
+		},
+		Entry("should allow update when immutable settings are unchanged",
+			&core.Seed{
+				Spec: core.SeedSpec{
+					Backup: &core.SeedBackup{
+						ProviderConfig: &runtime.RawExtension{
+							Raw: []byte(`{"immutability":{"retentionType":"bucket","retentionPeriod":"96h"}}`),
+						},
+					},
+				},
+			},
+			&core.Seed{
+				Spec: core.SeedSpec{
+					Backup: &core.SeedBackup{
+						ProviderConfig: &runtime.RawExtension{
+							Raw: []byte(`{"immutability":{"retentionType":"bucket","retentionPeriod":"96h"}}`),
+						},
+					},
+				},
+			},
+			nil,
+		),
+		Entry("should not allow update when retention period is reduced",
+			&core.Seed{
+				Spec: core.SeedSpec{
+					Backup: &core.SeedBackup{
+						ProviderConfig: &runtime.RawExtension{
+							Raw: []byte(`{"immutability":{"retentionType":"bucket","retentionPeriod":"96h"}}`),
+						},
+					},
+				},
+			},
+			&core.Seed{
+				Spec: core.SeedSpec{
+					Backup: &core.SeedBackup{
+						ProviderConfig: &runtime.RawExtension{
+							Raw: []byte(`{"immutability":{"retentionType":"bucket","retentionPeriod":"48h"}}`),
+						},
+					},
+				},
+			},
+			errors.New("reducing the retention period from 96h0m0s to 48h0m0s is not allowed"),
+		),
+		Entry("should not allow update when retention type is changed",
+			&core.Seed{
+				Spec: core.SeedSpec{
+					Backup: &core.SeedBackup{
+						ProviderConfig: &runtime.RawExtension{
+							Raw: []byte(`{"immutability":{"retentionType":"bucket","retentionPeriod":"96h"}}`),
+						},
+					},
+				},
+			},
+			&core.Seed{
+				Spec: core.SeedSpec{
+					Backup: &core.SeedBackup{
+						ProviderConfig: &runtime.RawExtension{
+							Raw: []byte(`{"immutability":{"retentionType":"bar","retentionPeriod":"96h"}}`),
+						},
+					},
+				},
+			},
+			errors.New("modifying the retention type from 'bucket' to 'bar' is not allowed"),
+		),
+		Entry("should not allow disabling immutable settings",
+			&core.Seed{
+				Spec: core.SeedSpec{
+					Backup: &core.SeedBackup{
+						ProviderConfig: &runtime.RawExtension{
+							Raw: []byte(`{"immutability":{"retentionType":"bucket","retentionPeriod":"96h"}}`),
+						},
+					},
+				},
+			},
+			&core.Seed{
+				Spec: core.SeedSpec{
+					Backup: &core.SeedBackup{
+						ProviderConfig: nil,
+					},
+				},
+			},
+			errors.New("disabling immutable settings is not allowed"),
+		),
+	)
+
+	var _ = DescribeTable("ValidateCreate",
+		func(newSeed *core.Seed, expectedError error) {
+			err := seedValidator.Validate(context.Background(), newSeed, nil)
+			if expectedError != nil {
+				Expect(err).To(HaveOccurred())
+				fmt.Println(err)
+				fmt.Println(expectedError)
+				Expect(err.Error()).Should(ContainSubstring(expectedError.Error()))
+			} else {
+				Expect(err).NotTo(HaveOccurred())
+			}
+		},
+		Entry("should allow creation with valid immutable settings",
+			&core.Seed{
+				Spec: core.SeedSpec{
+					Backup: &core.SeedBackup{
+						ProviderConfig: &runtime.RawExtension{
+							Raw: []byte(`{"immutability":{"retentionType":"bucket","retentionPeriod":"96h"}}`),
+						},
+					},
+				},
+			},
+			nil,
+		),
+		Entry("should not allow creation with invalid retention type",
+			&core.Seed{
+				Spec: core.SeedSpec{
+					Backup: &core.SeedBackup{
+						ProviderConfig: &runtime.RawExtension{
+							Raw: []byte(`{"immutability":{"retentionType":"invalid","retentionPeriod":"96h"}}`),
+						},
+					},
+				},
+			},
+			errors.New("invalid retentionType 'invalid'; must be 'bucket'"),
+		),
+		Entry("should not allow creation with invalid retention period format",
+			&core.Seed{
+				Spec: core.SeedSpec{
+					Backup: &core.SeedBackup{
+						ProviderConfig: &runtime.RawExtension{
+							Raw: []byte(`{"immutability":{"retentionType":"bucket","retentionPeriod":"invalid"}}`),
+						},
+					},
+				},
+			},
+			errors.New("error parsing immutable settings: error parsing immutable settings: invalid duration: time: invalid duration \"invalid\""),
+		),
+		Entry("should not allow creation with negative retention period",
+			&core.Seed{
+				Spec: core.SeedSpec{
+					Backup: &core.SeedBackup{
+						ProviderConfig: &runtime.RawExtension{
+							Raw: []byte(`{"immutability":{"retentionType":"bucket","retentionPeriod":"-96h"}}`),
+						},
+					},
+				},
+			},
+			errors.New("retentionPeriod must be greater than zero"),
+		),
+		Entry("should allow creation without immutable settings",
+			&core.Seed{
+				Spec: core.SeedSpec{
+					Backup: &core.SeedBackup{
+						ProviderConfig: nil,
+					},
+				},
+			},
+			nil,
+		),
+	)
+
+})
